ccc89479b626e2de0d1dba851312c8f939ef8e6f
Aleksander Figiel <olek.figiel@gmail.com>
2020-06-08 23:06:42 +0200

    Add files via upload

--- barcode/barcode.js
+++ barcode/barcode.js
@@ -68,13 +68,57 @@ var barcode = function() {
 		elements.canvasg = document.querySelector(config.canvasg);
 		elements.ctxg = elements.canvasg.getContext('2d');
 
-		if (navigator.getUserMedia) {
+		/*if (navigator.getUserMedia) {
 			navigator.getUserMedia({audio: false, video: true}, function(stream) {
-				elements.video.src = window.URL.createObjectURL(stream);
+				elements.video.srcObject = stream;
 			}, function(error) {
 				console.log(error);
 			});
+		}*/
+
+/*********teste na biblioteca do cara para abertura da camera*********/
+		if (navigator.mediaDevices === undefined) {
+		  navigator.mediaDevices = {};
 		}
+
+		// Some browsers partially implement mediaDevices. We can't just assign an object
+		// with getUserMedia as it would overwrite existing properties.
+		// Here, we will just add the getUserMedia property if it's missing.
+		if (navigator.mediaDevices.getUserMedia === undefined) {
+		  navigator.mediaDevices.getUserMedia = function(constraints) {
+
+		    // First get ahold of the legacy getUserMedia, if present
+		    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+		    // Some browsers just don't implement it - return a rejected promise with an error
+		    // to keep a consistent interface
+		    if (!getUserMedia) {
+		      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+		    }
+
+		    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+		    return new Promise(function(resolve, reject) {
+		      getUserMedia.call(navigator, constraints, resolve, reject);
+		    });
+		  }
+		}
+
+		navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+		.then(function(stream) {
+		  var video = document.querySelector('video');
+		  // Older browsers may not have srcObject
+		  if ("srcObject" in video) {
+		    video.srcObject = stream;
+		  } else {
+		    // Avoid using this in new browsers, as it is going away.
+		    video.src = window.URL.createObjectURL(stream);
+		  }
+		  
+		})
+		.catch(function(err) {
+		  console.log(err.name + ": " + err.message);
+		});
+		/****fim do teste*****/
 
 		elements.video.addEventListener('canplay', function(e) {
 
