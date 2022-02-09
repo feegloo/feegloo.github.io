df76e34d8c2ff49cddc141ebe7ea0916b70cd02c
Aleksander Figiel <aleksander.figiel@rtbhouse.com>
2022-02-09 14:02:35 +0100

    update

---
+++ scripts/google-docs-export.sh
@@ -0,0 +1,6 @@
+curl \
+  'https://www.googleapis.com/drive/v2/files/1AP6wBUaiLK7k6D0QhHmPDpCbhq1ZL_9q3DSI2TZxDLM/export?mimeType=application%2Fpdf&key=AIzaSyDssEF2a7biRAFG8LGWRTTSXz6MLWXFgT0' \
+  --header 'Authorization2: Bearer [YOUR_ACCESS_TOKEN]' \
+  --header 'Accept: application/json' \
+  --compressed
+
