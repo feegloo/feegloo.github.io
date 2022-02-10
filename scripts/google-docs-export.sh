b954810440b3010d1f65904de2215ca932d72eb2
feegloo <olek.figiel@gmail.com>
2022-02-10 10:54:39 +0100

    Delete scripts directory

--- scripts/google-docs-export.sh
+++
@@ -1,6 +0,0 @@
-curl \
-  'https://www.googleapis.com/drive/v2/files/1AP6wBUaiLK7k6D0QhHmPDpCbhq1ZL_9q3DSI2TZxDLM/export?mimeType=application%2Fpdf&key=AIzaSyDssEF2a7biRAFG8LGWRTTSXz6MLWXFgT0' \
-  --header 'Authorization2: Bearer [YOUR_ACCESS_TOKEN]' \
-  --header 'Accept: application/json' \
-  --compressed
-
